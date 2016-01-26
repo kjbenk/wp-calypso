c2aece630fccb6c2f3195eacb4a4891ee2aa1f7c
Igor Klimer <igor.klimer@automattic.com>
2016-01-26 14:30:39 +0100

    Domains: trim and convert to lowercase values of DNS records

--- client/my-sites/upgrades/domain-management/dns/dns-add-new.jsx
+++ client/my-sites/upgrades/domain-management/dns/dns-add-new.jsx
@@ -110,7 +110,7 @@ const DnsAddNew = React.createClass( {
 		return ( event ) => {
 			this.formStateController.handleFieldChange( {
 				name: event.target.name,
-				value: event.target.value
+				value: event.target.value.trim().toLowerCase()
 			} );
 		};
 	},
