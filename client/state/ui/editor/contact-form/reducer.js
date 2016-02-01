092bec4f07644748f3578e10ee1af1fb16c6fd3e
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-02-01 14:18:28 -0300

    Test updated to make strict equality checks to test if constants or provided states are mutated by the reducer

--- client/state/ui/editor/contact-form/reducer.js
+++ client/state/ui/editor/contact-form/reducer.js
@@ -1,13 +1,25 @@
 /**
+ * External dependencies
+ */
+import cloneDeep from 'lodash/lang/cloneDeep';
+
+/**
  * Internal dependencies
  */
-import { CONTACT_FORM_LOAD_FORM, CONTACT_FORM_ADD_DEFAULT_FIELD, CONTACT_FORM_REMOVE_FIELD, CONTACT_FORM_CLEAR_FORM } from './action-types';
+import {
+	CONTACT_FORM_LOAD_FORM,
+	CONTACT_FORM_ADD_DEFAULT_FIELD,
+	CONTACT_FORM_REMOVE_FIELD,
+	CONTACT_FORM_CLEAR_FORM
+} from './action-types';
 import { CONTACT_FORM_DEFAULT, CONTACT_FORM_DEFAULT_NEW_FIELD } from './constants';
 
-export default function( state = CONTACT_FORM_DEFAULT, action ) {
+const initalState = cloneDeep( CONTACT_FORM_DEFAULT );
+
+export default function( state = initalState, action ) {
 	switch ( action.type ) {
 		case CONTACT_FORM_LOAD_FORM:
-			state = Object.assign( {}, action.contactForm );
+			state = cloneDeep( action.contactForm );
 			break;
 		case CONTACT_FORM_ADD_DEFAULT_FIELD:
 			state = Object.assign( {}, state, {
@@ -16,11 +28,11 @@ export default function( state = CONTACT_FORM_DEFAULT, action ) {
 			break;
 		case CONTACT_FORM_REMOVE_FIELD:
 			const { index } = action;
-			state = Object.assign( {}, state );
+			state = cloneDeep( state );
 			state.fields.splice( index, 1 );
 			break;
 		case CONTACT_FORM_CLEAR_FORM:
-			state = Object.assign( {}, CONTACT_FORM_DEFAULT );
+			state = cloneDeep( CONTACT_FORM_DEFAULT );
 			break;
 	}
 
--- client/state/ui/editor/contact-form/test/reducer.js
+++ client/state/ui/editor/contact-form/test/reducer.js
@@ -7,90 +7,163 @@ import { assert } from 'chai';
  * Internal dependencies
  */
 import reducer from '../reducer';
-import { CONTACT_FORM_DEFAULT, CONTACT_FORM_DEFAULT_NEW_FIELD } from '../constants';
+import { CONTACT_FORM_DEFAULT, CONTACT_FORM_FIELD_TYPES } from '../constants';
 import { CONTACT_FORM_LOAD_FORM, CONTACT_FORM_ADD_DEFAULT_FIELD, CONTACT_FORM_REMOVE_FIELD, CONTACT_FORM_CLEAR_FORM } from '../action-types';
 
 describe( "editor's contact form state reducer", () => {
+
 	it( 'should return the default contact form when neither state nor action is provided', () => {
 		const state = reducer( undefined, {} );
 
 		assert.deepEqual( state, CONTACT_FORM_DEFAULT );
+		assert.notStrictEqual( state, CONTACT_FORM_DEFAULT, 'returned state is strictly equal to CONTACT_FORM_DEFAULT.' );
 	} );
 
-	it( "should add the default new field to the state's fields list", () => {
-		let contactForm = Object.freeze( {
-			fields: [
-				{ label: 'Name' },
-				{ label: 'Email' }
-			]
-		} );
+	describe( 'load form', () => {
+		it( 'should override the state with the provided contact form', () => {
+			const contactForm = {
+				to: 'user@example.com',
+				subject: 'here be dragons',
+				fields: [
+					{ label: 'Name' },
+					{ label: 'Email' },
+					{ label: 'Website' },
+					{ label: 'Comment' }
+				]
+			};
 
-		const state = reducer( contactForm, {
-			type: CONTACT_FORM_CLEAR_FORM
-		} );
+			const state = reducer( null, {
+				type: CONTACT_FORM_LOAD_FORM,
+				contactForm
+			} );
 
-		assert.deepEqual( state, CONTACT_FORM_DEFAULT );
+			assert.deepEqual( state, contactForm );
+			assert.notStrictEqual( state, contactForm, 'the returned state and contactForm are strictly equal.' );
+			assert.notStrictEqual( state.fields, contactForm.fields, 'fields on the returned state and contactForm are strictly equal.' );
+		} );
 	} );
 
-	it( 'should initialize the state with the provided contact form', () => {
-		const contactForm = Object.freeze( {
-			to: 'user@example.com',
-			subject: 'here be dragons',
-			fields: [
+	describe( 'add default field', () => {
+		it( "should add the default new field to the provided state", () => {
+			const contactForm = { fields: [
 				{ label: 'Name' },
 				{ label: 'Email' },
 				{ label: 'Website' },
 				{ label: 'Comment' }
-			]
-		} );
+			] };
 
-		const state = reducer( null, {
-			type: CONTACT_FORM_LOAD_FORM,
-			contactForm
-		} );
+			const state = reducer( contactForm, {
+				type: CONTACT_FORM_ADD_DEFAULT_FIELD
+			} );
 
-		assert.deepEqual( state, contactForm );
-	} );
-
-	it( "should add the default new field to the state's fields list", () => {
-		let contactForm = Object.freeze( {
-			fields: [
+			assert.deepEqual( state, { fields: [
+				{ label: 'Name' },
+				{ label: 'Email' },
+				{ label: 'Website' },
+				{ label: 'Comment' },
+				{ label: 'Text', type: 'text' }
+			] } );
+			assert.deepEqual( contactForm, { fields: [
 				{ label: 'Name' },
 				{ label: 'Email' },
 				{ label: 'Website' },
 				{ label: 'Comment' }
-			]
+			] } );
+			assert.notStrictEqual( state, contactForm, 'the returned state and the provided state are strictly equal.' );
+			assert.notStrictEqual( state.fields, contactForm.fields, 'the fields on the returned state and the provided state are strictly equal.' );
 		} );
 
-		const state = reducer( contactForm, {
-			type: CONTACT_FORM_ADD_DEFAULT_FIELD
-		} );
-		contactForm.fields.push( CONTACT_FORM_DEFAULT_NEW_FIELD );
+		it( 'should add the default new field to the inital state when no state is provided', () => {
+			const state = reducer( undefined, { type: CONTACT_FORM_ADD_DEFAULT_FIELD } );
 
-		assert.deepEqual( state, contactForm );
+			assert.deepEqual( state, { fields: [
+				{ label: 'Name', type: 'name', required: true },
+				{ label: 'Email', type: 'email', required: true },
+				{ label: 'Website', type: 'url' },
+				{ label: 'Comment', type: 'textarea', required: true },
+				{ label: 'Text', type: 'text' }
+			] } );
+			assert.deepEqual( CONTACT_FORM_DEFAULT, { fields: [
+				{ label: 'Name', type: 'name', required: true },
+				{ label: 'Email', type: 'email', required: true },
+				{ label: 'Website', type: 'url' },
+				{ label: 'Comment', type: 'textarea', required: true }
+			] } );
+		} );
 	} );
 
-	it( "should remove a field from the state's fields list", () => {
-		const contactForm = Object.freeze( {
-			fields: [
-				{ label: 'Name' },
-				{ label: 'Email' },
-				{ label: 'Website' },
-				{ label: 'Comment' }
-			]
+	describe( 'remove field', () => {
+		it( "should remove a field from the provided state by index", () => {
+			const contactForm = {
+				fields: [
+					{ label: 'Name' },
+					{ label: 'Email' },
+					{ label: 'Website' },
+					{ label: 'Comment' }
+				]
+			};
+
+			const state = reducer( contactForm, {
+				type: CONTACT_FORM_REMOVE_FIELD,
+				index: 2
+			} );
+
+			assert.deepEqual( state, {
+				fields: [
+					{ label: 'Name' },
+					{ label: 'Email' },
+					{ label: 'Comment' }
+				]
+			} );
+			assert.deepEqual( contactForm, {
+				fields: [
+					{ label: 'Name' },
+					{ label: 'Email' },
+					{ label: 'Website' },
+					{ label: 'Comment' }
+				]
+			}, 'provided state were mutated.' );
 		} );
 
-		const state = reducer( contactForm, {
-			type: CONTACT_FORM_REMOVE_FIELD,
-			index: 2
+		it( "should not mutate the inital state when no state is provided", () => {
+			const state = reducer( undefined, {
+				type: CONTACT_FORM_REMOVE_FIELD,
+				index: 2
+			} );
+
+			assert.deepEqual( state, {
+				fields: [
+					{ label: 'Name', type: 'name', required: true },
+					{ label: 'Email', type: 'email', required: true },
+					{ label: 'Comment', type: 'textarea', required: true }
+				]
+			} );
+			assert.deepEqual( CONTACT_FORM_DEFAULT, {
+				fields: [
+					{ label: 'Name', type: CONTACT_FORM_FIELD_TYPES.name, required: true },
+					{ label: 'Email', type: CONTACT_FORM_FIELD_TYPES.email, required: true },
+					{ label: 'Website', type: CONTACT_FORM_FIELD_TYPES.url },
+					{ label: 'Comment', type: CONTACT_FORM_FIELD_TYPES.textarea, required: true }
+				]
+			}, 'contact form default values were mutated.' );
 		} );
 
-		assert.deepEqual( state, {
-			fields: [
-				{ label: 'Name' },
-				{ label: 'Email' },
-				{ label: 'Comment' }
-			]
+	} );
+
+	describe( 'reset global state', () => {
+		it( "should add the default new field to the state's fields list", () => {
+			const state = reducer( {
+				fields: [
+					{ label: 'Name' },
+					{ label: 'Email' }
+				]
+			}, {
+				type: CONTACT_FORM_CLEAR_FORM
+			} );
+
+			assert.deepEqual( state, CONTACT_FORM_DEFAULT );
+			assert.notStrictEqual( state, CONTACT_FORM_DEFAULT, 'the returned state and the default contact form are strictly equal.' );
+			assert.notStrictEqual( state.fields, CONTACT_FORM_DEFAULT.fields, 'the fields on the returned state and the default contact form are strictly equal.' );
 		} );
 	} );
 } );
