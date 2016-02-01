7734744acbfdd21eae86d41d4289f65775da4083
bluefuton <chris@bluefuton.com>
2016-02-02 11:08:17 +1300

    Stats: add stat for open/close of lists and tags expandable menus

--- client/state/ui/reader/sidebar/reducer.js
+++ client/state/ui/reader/sidebar/reducer.js
@@ -8,10 +8,19 @@ import { combineReducers } from 'redux';
  */
 import { READER_SIDEBAR_LISTS_TOGGLE, READER_SIDEBAR_TAGS_TOGGLE } from 'state/action-types';
 
+const stats = require( 'reader/stats' );
+
 function isListsOpen( state = false, action ) {
 	switch ( action.type ) {
 		case READER_SIDEBAR_LISTS_TOGGLE:
 			state = ! state;
+			if ( state ) {
+				stats.recordAction( 'sidebar_open_lists_menu' );
+				stats.recordGaEvent( 'Clicked Open Lists Menu' );
+			} else {
+				stats.recordAction( 'sidebar_close_lists_menu' );
+				stats.recordGaEvent( 'Clicked Close Lists Menu' );
+			}
 			break;
 	}
 
@@ -22,6 +31,13 @@ function isTagsOpen( state = false, action ) {
 	switch ( action.type ) {
 		case READER_SIDEBAR_TAGS_TOGGLE:
 			state = ! state;
+			if ( state ) {
+				stats.recordAction( 'sidebar_open_tags_menu' );
+				stats.recordGaEvent( 'Clicked Open Tags Menu' );
+			} else {
+				stats.recordAction( 'sidebar_close_tags_menu' );
+				stats.recordGaEvent( 'Clicked Close Tags Menu' );
+			}
 			break;
 	}
 
