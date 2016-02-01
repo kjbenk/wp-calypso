103c5c8a5bea51ab35d404ecbc95b0b60e00e920
bluefuton <chris@bluefuton.com>
2016-02-02 12:58:55 +1300

    Move sidebar toggle stats into action

--- client/state/ui/reader/sidebar/reducer.js
+++ client/state/ui/reader/sidebar/reducer.js
@@ -8,19 +8,10 @@ import { combineReducers } from 'redux';
  */
 import { READER_SIDEBAR_LISTS_TOGGLE, READER_SIDEBAR_TAGS_TOGGLE } from 'state/action-types';
 
-const stats = require( 'reader/stats' );
-
 function isListsOpen( state = false, action ) {
 	switch ( action.type ) {
 		case READER_SIDEBAR_LISTS_TOGGLE:
 			state = ! state;
-			if ( state ) {
-				stats.recordAction( 'sidebar_open_lists_menu' );
-				stats.recordGaEvent( 'Clicked Open Lists Menu' );
-			} else {
-				stats.recordAction( 'sidebar_close_lists_menu' );
-				stats.recordGaEvent( 'Clicked Close Lists Menu' );
-			}
 			break;
 	}
 
@@ -31,13 +22,6 @@ function isTagsOpen( state = false, action ) {
 	switch ( action.type ) {
 		case READER_SIDEBAR_TAGS_TOGGLE:
 			state = ! state;
-			if ( state ) {
-				stats.recordAction( 'sidebar_open_tags_menu' );
-				stats.recordGaEvent( 'Clicked Open Tags Menu' );
-			} else {
-				stats.recordAction( 'sidebar_close_tags_menu' );
-				stats.recordGaEvent( 'Clicked Close Tags Menu' );
-			}
 			break;
 	}
 
